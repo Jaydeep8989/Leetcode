26b07ca1e9861bd657f30d88c68cdb810b1c1ed0
jaydeep8989 <jaydeephirparah@gmail.com>
2024-04-25 21:47:37 +0530

    Leetcode 1137 solved.

---
+++ LeetCode_1137.js
@@ -0,0 +1,34 @@
+// The Tribonacci sequence Tn is defined as follows: 
+
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+// Given n, return the value of Tn.
+
+ 
+
+// Example 1:
+
+// Input: n = 4
+// Output: 4
+// Explanation:
+// T_3 = 0 + 1 + 1 = 2
+// T_4 = 1 + 1 + 2 = 4
+// Example 2:
+
+// Input: n = 25
+// Output: 1389537
+
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    let t = [0,1,1];
+    if(n === 0) return 0;
+    if(n === 1 || n === 2) return 1;
+    for (let i = 3; i <= n; i++){
+        t[i] = t[i-1] + t[i-2] + t[i-3]
+    }
+    return t[n];
+};
